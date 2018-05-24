9bceee1aee39effbe63a97897badd3979c8f829f
kcooler <kcooler@gmail.com>
2018-05-24 15:07:30 -0400

    Submission of day 4 exercises

--- m1-w1-d4-loops-and-arrays-exercises/src/main/java/com/techelevator/Exercises.java
+++ m1-w1-d4-loops-and-arrays-exercises/src/main/java/com/techelevator/Exercises.java
@@ -10,6 +10,7 @@ public class Exercises {
 	 firstLast6([13, 6, 1, 2, 3]) → false
 	 */
 	public boolean firstLast6(int[] nums) {
+		if (nums[0] == 6 || nums[nums.length - 1] == 6) return true;
 		return false;
 	}
 
@@ -21,6 +22,7 @@ public class Exercises {
 	 sameFirstLast([1, 2, 1]) → true
 	 */
 	public boolean sameFirstLast(int[] nums) {
+		if (nums.length >= 1 && (nums[0] == nums[nums.length - 1])) return true;
 		return false;
 	}
 
@@ -29,7 +31,8 @@ public class Exercises {
 	 makePi() → [3, 1, 4]
 	 */
 	public int[] makePi() {
-		return new int[] {};
+		int[] piFirst3 = {3, 1, 4};
+		return piFirst3;
 	}
 
 	/*
@@ -40,6 +43,7 @@ public class Exercises {
 	 commonEnd([1, 2, 3], [1, 3]) → true
 	 */
 	public boolean commonEnd(int[] a, int[] b) {
+		if (a[0] == b[0] || a[a.length - 1] == b[b.length -1]) return true;
 		return false;
 	}
 
@@ -50,7 +54,11 @@ public class Exercises {
 	 sum3([7, 0, 0]) → 7
 	 */
 	public int sum3(int[] nums) {
-		return 0;
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+		}
+		return sum;
 	}
 
 	/*
@@ -61,7 +69,12 @@ public class Exercises {
 	 rotateLeft3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] rotateLeft3(int[] nums) {
-		return new int[] {};
+		int[] newArray = new int[3];
+		newArray[0] = nums[1];
+		newArray[1] = nums[2];
+		newArray[2] = nums[0];
+		
+		return newArray;
 	}
 
 	/*
@@ -72,7 +85,11 @@ public class Exercises {
 	 reverse3([7, 0, 0]) → [0, 0, 7]
 	 */
 	public int[] reverse3(int[] nums) {
-		return new int[] {};
+		int[] newArray = new int[3];
+		newArray[0] = nums[2];
+		newArray[1] = nums[1];
+		newArray[2] = nums[0];
+		return newArray;
 	}
 
 	/*
@@ -83,7 +100,10 @@ public class Exercises {
 	 maxEnd3([2, 11, 3]) → [3, 3, 3]
 	 */
 	public int[] maxEnd3(int[] nums) {
-		return new int[] {};
+		int maxNum = nums[0];
+		if (nums[2] > maxNum) maxNum = nums[2];
+		int[] newArray = {maxNum, maxNum, maxNum};
+		return newArray;
 	}
 
 	/*
@@ -94,7 +114,11 @@ public class Exercises {
 	 sum2([1, 1, 1, 1]) → 2
 	 */
 	public int sum2(int[] nums) {
-		return 0;
+		if (nums.length < 2) {
+			if (nums.length == 1) return nums[0];
+			return 0;
+		}
+		return nums[0] + nums[1];
 	}
 
 	/*
@@ -105,7 +129,7 @@ public class Exercises {
 	 middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
 	 */
 	public int[] middleWay(int[] a, int[] b) {
-		return new int[] {};
+		return new int[] {a[1], b[1]};
 	}
 
 	/*
@@ -116,7 +140,11 @@ public class Exercises {
 	 countEvens([1, 3, 5]) → 0
 	 */
 	public int countEvens(int[] nums) {
-		return 0;
+		int count = 0;
+		for (int i = 0; i < nums.length; i ++) {
+			if (nums[i] % 2 == 0) count ++;
+		}
+		return count;
 	}
 
 	/*
@@ -128,7 +156,15 @@ public class Exercises {
 	 sum13([1, 2, 2, 1, 13]) → 6
 	 */
 	public int sum13(int[] nums) {
-		return 0;
+		int theSum = 0;
+		for (int i = 0; i < nums.length; i ++) {
+			if (nums[i] == 13) {
+				i++;
+				continue;
+			}
+			theSum += nums[i];
+		}
+		return theSum;
 	}
 
 	/*
@@ -138,6 +174,11 @@ public class Exercises {
 	 has22([2, 1, 2]) → false
 	 */
 	public boolean has22(int[] nums) {
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == 2) {
+				if (nums[i] == nums[i + 1]) return true;
+			}
+		}
 		return false;
 	}
 	
@@ -148,7 +189,10 @@ public class Exercises {
 	 lucky13([1, 2, 4]) → false
 	 */
 	public boolean lucky13(int[] nums) {
-		return false;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1 || nums[i] == 3) return false;
+		}
+		return true;
 	}
 
 	/*
@@ -158,6 +202,11 @@ public class Exercises {
 	 sum28([1, 2, 3, 4]) → false
 	 */
 	public boolean sum28(int[] nums) {
+		int sum2s = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 2) sum2s += nums[i];
+		}
+		if (sum2s == 8) return true;
 		return false;
 	}
 
