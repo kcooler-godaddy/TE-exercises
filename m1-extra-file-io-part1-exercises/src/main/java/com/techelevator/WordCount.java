7b14b3e82e03f7e1de17ac97cc4aaad8cf82e5d4
Brian Lauvray <lauvray.1@osu.edu>
2018-06-11 19:13:55 -0400

    added file IO Part 1

---
+++ m1-extra-file-io-part1-exercises/src/main/java/com/techelevator/WordCount.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class WordCount {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ m1-w4d2-file-io-part1-exercises/src/main/java/com/techelevator/WordSearch.java
@@ -0,0 +1,10 @@
+package com.techelevator;
+
+public class WordSearch {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
