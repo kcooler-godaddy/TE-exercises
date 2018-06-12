281bc46dbba9c26653e9a81c2d812fd54fef77d4
kcooler <kcooler@gmail.com>
2018-06-11 21:17:08 -0400

    completed kataRomanNumerals

--- m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataRomanNumerals.java
+++ m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataRomanNumerals.java
@@ -1,6 +1,92 @@
 package com.techelevator;
+import java.util.LinkedHashMap;
 
 public class KataRomanNumerals {
+	
+	public LinkedHashMap<String, Integer> getRomanValues(){
+		LinkedHashMap<String, Integer> romanValues = new LinkedHashMap<String, Integer>();
+		romanValues.put("M", 1000);
+		romanValues.put("CM", 900);
+		romanValues.put("D", 500);
+		romanValues.put("CD", 400);
+		romanValues.put("C", 100);
+		romanValues.put("XC", 90);
+		romanValues.put("L", 50);
+		romanValues.put("XL", 40); 
+		romanValues.put("X", 10);
+		romanValues.put("IX", 9);
+		romanValues.put("V", 5); 
+		romanValues.put("IV", 4);
+		romanValues.put("I", 1);
+		
+		return romanValues;
+	}
+	
+	
+	public int toArabic(String inputNumeral) {
+		int arabic = 0;
+		LinkedHashMap<String, Integer> romanValues = getRomanValues();
+		
+		for (String keyNumeral: romanValues.keySet()) {
+			while(inputNumeral.startsWith(keyNumeral)) {
+				
+					arabic += romanValues.get(keyNumeral);
+					if(inputNumeral.length() - keyNumeral.length() == 0) {
+						inputNumeral = "";
+						break;
+					}
+					inputNumeral = inputNumeral.substring(0 + keyNumeral.length());
+			}
+		}
+		
+		
+		return arabic;
+	}
+		
+		
+	
+	public String toRomanNumerals(int arabic) {
+		String romanNumerals = "";
+		
+		LinkedHashMap<String, Integer> romanCounters = getRomanCounters(arabic);
+		
+		for (String numeral: romanCounters.keySet()) {
+
+			for(int i = 0; i < romanCounters.get(numeral); i++) {
+				romanNumerals += numeral;		
+			}
+		}
+
+		return romanNumerals;
+	}
+	
+	public LinkedHashMap<String, Integer> getRomanCounters(int arabic){
+		LinkedHashMap<String, Integer> romanCounters = new LinkedHashMap<String, Integer>();
+		romanCounters.put("M", 0);
+		romanCounters.put("CM", 0);
+		romanCounters.put("D", 0);
+		romanCounters.put("CD", 0);
+		romanCounters.put("C", 0);
+		romanCounters.put("XC", 0);
+		romanCounters.put("L", 0);
+		romanCounters.put("XL", 0);
+		romanCounters.put("X", 0);
+		romanCounters.put("IX", 0);
+		romanCounters.put("V", 0);
+		romanCounters.put("IV", 0);
+		romanCounters.put("I", 0);
+		
+		LinkedHashMap<String, Integer> romanValues = getRomanValues(); 
+		
+		for(String numeral: romanValues.keySet()) {
+			while(arabic - romanValues.get(numeral) >= 0) {
+				romanCounters.put(numeral,  romanCounters.get(numeral) + 1);
+				arabic -= romanValues.get(numeral);
+			}
+		}
+		
+		return romanCounters;
+	}
 	
 	
 
--- m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataRomanNumeralsTest.java
+++ m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataRomanNumeralsTest.java
@@ -3,6 +3,94 @@ package com.techelevator;
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.util.LinkedHashMap;
+
 public class KataRomanNumeralsTest {
+	
+	KataRomanNumerals kata;
+	
+	@Before
+	public void setup() {
+		kata = new KataRomanNumerals();
+	}
+	
+	@Test
+	public void returns_I_for_1(){
+		Assert.assertEquals("I",  kata.toRomanNumerals(1));
+	}
+	@Test
+	public void returns_V_for_5(){
+		Assert.assertEquals("V",  kata.toRomanNumerals(5));
+	}
+	@Test
+	public void returns_X_for_10(){
+		Assert.assertEquals("X",  kata.toRomanNumerals(10));
+	}
+	@Test
+	public void returns_L_for_50(){
+		Assert.assertEquals("L",  kata.toRomanNumerals(50));
+	}
+	@Test
+	public void returns_C_for_100(){
+		Assert.assertEquals("C",  kata.toRomanNumerals(100));
+	}
+	@Test
+	public void returns_D_for_500(){
+		Assert.assertEquals("D",  kata.toRomanNumerals(500));
+	}
+	@Test
+	public void returns_M_for_1000(){
+		Assert.assertEquals("M",  kata.toRomanNumerals(1000));
+	}
+	
+	@Test
+	public void returns_map_of_roman_numeral_counts_in_263() {
+		int arabic = 263;
+		LinkedHashMap<String, Integer> tester = new LinkedHashMap<String, Integer>();
+		tester.put("M", 0);
+		tester.put("CM", 0);
+		tester.put("D", 0);
+		tester.put("CD", 0);
+		tester.put("C", 2);
+		tester.put("XC", 0);
+		tester.put("L", 1);
+		tester.put("XL", 0);
+		tester.put("X", 1); 
+		tester.put("IX", 0); 
+		tester.put("V", 0);
+		tester.put("IV", 0);
+		tester.put("I", 3);
+		
+		Assert.assertEquals(tester, kata.getRomanCounters(arabic));
+	}
+	@Test
+	public void returns_IV_for_4(){
+		Assert.assertEquals("IV",  kata.toRomanNumerals(4));
+	}
+	@Test
+	public void returns_IX_for_9(){
+		Assert.assertEquals("IX",  kata.toRomanNumerals(9));
+	}
+	@Test
+	public void returns_CCCLXIX_for_369(){
+		Assert.assertEquals("CCCLXIX",  kata.toRomanNumerals(369));
+	}
+	@Test
+	public void returns_MCMXCVIII_for_1998(){
+		Assert.assertEquals("MCMXCVIII",  kata.toRomanNumerals(1998));
+	}
+	
+	@Test
+	public void returns_1000_for_M(){
+		Assert.assertEquals(new Integer(1000), new Integer(kata.toArabic("M")));
+	}
+	@Test
+	public void returns_1998_for_MCMXCVIII(){
+		Assert.assertEquals(new Integer(1998), new Integer(kata.toArabic("MCMXCVIII")));
+	}
+	@Test
+	public void returns_369_for_CCCLXIX(){
+		Assert.assertEquals(new Integer(369), new Integer(kata.toArabic("CCCLXIX")));
+	}
 
 }
