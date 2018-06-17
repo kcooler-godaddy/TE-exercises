18d3758b7554d337a0eaae605f72d231051980f4
Brian Lauvray <lauvray.1@osu.edu>
2018-06-17 15:16:34 -0400

    Added intro to databases

---
+++ m2-w5d1-intro-to-databases-exercises/intro-to-databases-exercises.sql
@@ -0,0 +1,49 @@
+-- The following queries utilize the "world" database.
+-- Write queries for the following problems. 
+-- Notes:
+--   GNP is expressed in millions of US Dollars
+--   The value immediately after the problem statement is the expected number of rows that should be returned by the query.
+
+-- 1. The name and population of all cities in Ontario, Canada (27 rows)
+
+-- 2. The name and population of all cities in Montana (1 row)
+
+-- 3. The name, form of government, and head of state of all countries in Europe (46 rows)
+
+-- 4. The name, population, surface area, and average life expectancy of all countries in Asia (51 rows)
+
+-- 5. The name, country code, and population of all cities with a population greater than 8 million people (10 rows)
+
+-- 6. The name, country code, and population of all cities with a population less than one thousand people (11 rows)
+
+-- 7. The name, continent, and GNP of all countries with a GNP greater than one trillion dollars (6 rows)
+
+-- 8. The name, continent, population, GNP, and average life expectancy of all countries with an average life expectancy greater than 80 years (5 rows)
+
+-- 9. The name and population of all cities in the USA with a population of greater than 1 million people (9 rows)
+
+-- 10. The name and population of all cities in China with a population of greater than 1 million people (35 rows)
+
+-- 11. The name and region of all countries in North or South America (51 rows)
+
+-- 12. The name, continent, and head of state of all countries whose form of government is a monarchy (43 rows)
+
+-- 13. The name of all cities in the USA with a population between 1 million and 2 million people (6 rows) 
+
+-- 14. The name and region of all countries in North or South America except for countries in the Caribbean (27 rows)
+
+-- 15. The name, population, and GNP of all countries with a GNP greater than $1 trillion dollars and a population of less than 100 million people (4 rows)
+
+-- 16. The name and population of all cities in Texas that have a population of greater than 1 million people (3 rows)
+
+-- 17. The name and average life expectancy of all countries on the continent of Oceania (28 rows)
+
+-- 18. The name and average life expectancy of all countries on the continent of Oceania for which an average life expectancy has been provided (i.e. not equal to null) (20 rows)
+
+-- 19. The name of all countries on the continent of Oceania for which an average life expectancy has not been provided (i.e. equal to null) (8 rows)
+
+-- 20. The name, continent, GNP, and average life expectancy of all countries that have an average life expectancy of at least 70 years and a GNP between $1 million and $100 million dollars (3 rows)
+
+-- 21. The per capita GNP (i.e. GNP divided by population) in US Dollars of all countries in Europe (46 rows)
+
+-- 22. The number of years since independence for all countries that have a year of independence (192 rows)
