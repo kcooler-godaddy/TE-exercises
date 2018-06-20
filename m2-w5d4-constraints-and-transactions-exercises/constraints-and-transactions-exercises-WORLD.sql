5da343d6d85e6a27d216e2b027c7648846780bd2
Brian Lauvray <lauvray.1@osu.edu>
2018-06-20 16:47:32 -0400

    Added constraints and transactions

---
+++ m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises-WORLD.sql
@@ -0,0 +1,34 @@
+-- Write queries to return the following:
+-- Make the following changes in the "world" database.
+
+-- 1. Add Superman's hometown, Smallville, Kansas to the city table. The 
+-- countrycode is 'USA', and population of 45001. (Yes, I looked it up on 
+-- Wikipedia.)
+
+-- 2. Add Kryptonese to the countrylanguage table. Kryptonese is spoken by 0.0001
+-- percentage of the 'USA' population.
+
+-- 3. After heated debate, "Kryptonese" was renamed to "Krypto-babble", change 
+-- the appropriate record accordingly.
+
+-- 4. Set the US captial to Smallville, Kansas in the country table.
+
+-- 5. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+
+-- 6. Return the US captial to Washington.
+
+-- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+
+-- 8. Reverse the "is the official language" setting for all languages where the
+-- country's year of independence is within the range of 1800 and 1972 
+-- (exclusive). 
+-- (590 rows affected)
+
+-- 9. Convert population so it is expressed in 1,000s for all cities. (Round to
+-- the nearest integer value greater than 0.)
+-- (4079 rows affected)
+
+-- 10. Assuming a country's surfacearea is expressed in miles, convert it to 
+-- meters for all countries where French is spoken by more than 20% of the 
+-- population.
+-- (7 rows affected)
