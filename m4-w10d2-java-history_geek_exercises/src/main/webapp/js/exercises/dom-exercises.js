6e44332d09c5a5fa7b2dbc2f6eb0c035914930e8
kcooler <kcooler@gmail.com>
2018-07-24 14:21:21 -0400

    completed exercises

--- m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/dom-exercises.js
+++ m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/dom-exercises.js
@@ -1,45 +1,53 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
 //Locate all <div> elements and add the 'president' class.
 function presentialDivs() {
-
+	$("#presidents").children("div").addClass("president");
 }
 
 //Locate George Washington and add the 'founding-father' class
 function georgeWashington() {
-
+	$("#george-washington").addClass("founding-father");	
 }
 
 //Locate all Republican presidents and add the 'red' class.
 function republicanPresidents() {
-
+	$(".Republican").addClass("red");
 }
 
 //Locate all Democrat presidents and add the 'blue' class.
 function democratPresidents() {
-
+	$(".Democratic").addClass("blue");
 }
 
 //Locate all Federalist presidents and add the 'gold' class.
 function otherPresidents() {
-
+	$(".president").not(".Democratic").not(".Republican").addClass("gold");
 }
 
 //Locate all presidents named 'James' and add the 'james' class.
 function presidentsNamedJames() {
-
+	$(".president[id^='james']").addClass("james");
 }
 
 //Locate each president serving at the beginning of each century and add the 'beginningOfCentury' class.
 function turnOfTheCenturyPresidents() {
-
+	$("#presidents").children("h2").next().addClass("beginningOfCentury");	
 }
 
 //Locate each president serving at the end of the century and add the 'endOfCentury' class.
 function endOfTheCenturyPresidents() {
-    
+	$("#presidents").children("h2").prev().addClass("endOfCentury");
+	$("#presidents").children().last().addClass("endOfCentury");
 }
 
 //Locate all presidents serving in the 1900s who appear on currency and add the 'appearOnCurrency' class.
 function currencyPresidents() {
-
+	$("#theodore-roosevelt").prev().nextAll().filter(".currency").addClass("appearOnCurrency");
 }
+
+//.prev().nextall().filter(".currency")
+
+
+
+
+
